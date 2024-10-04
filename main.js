2d9f137d34bcde5bdf52b8e8cc6d185d076ec4f2
pcapo_dawi <pcapolozano@cifpfbmoll.eu>
2024-10-04 17:30:53 +0200

    Second commit

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>¡Hola mundo! Soy Pedro Capó</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
@@ -19,6 +19,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
 setupCounter(document.querySelector('#counter'))
